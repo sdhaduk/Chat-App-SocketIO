fc678d2965a5a205d9a890eca1f6eca11267cecb
sdhaduk <sagardhaduk1@gmail.com>
2023-11-16 17:17:14 -0600

    connected server to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,19 @@ import "dotenv/config";
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => {
+    console.log("Connected to DB");
+    app.listen(process.env.PORT, (req, res) => {
+        console.log(`Server running on Port: ${process.env.PORT}`)
+    });
+})
+.catch((error) => {
+    console.log(error)
+});
+
+
+
+
+
