f3e8d170b4c3e430013a4e3d5ba8ac106ebab64c
sdhaduk <sagardhaduk1@gmail.com>
2023-11-17 13:19:05 -0600

    Added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,22 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav } from "react-bootstrap";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
     </>
   );
 }
---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Nav, Navbar, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NavBar: React.FC = () => {
+  return (
+    <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
+      <Container>
+        <h2>
+          <Link to="/" className="link-light text-decoration-none">ChatApp</Link>
+        </h2>
+        <span className="text-warning">Logged in as Sagar</span>
+        <Nav>
+            <Stack direction="horizontal" gap={3}>
+                <Link to="/login" className="link-light text-decoration-none">Login</Link>
+                <Link to="/register" className="link-light text-decoration-none">Register</Link>
+            </Stack>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
