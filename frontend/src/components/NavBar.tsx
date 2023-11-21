10072b94eb7ea525c28f676e3b2522c63f57f26b
sdhaduk <sagardhaduk1@gmail.com>
2023-11-20 22:26:12 -0600

    added conditonal rendering to navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Nav, Navbar, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const NavBar: React.FC = () => {
+  const { user, logoutUser } = useContext(AuthContext);
+
   return (
     <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
       <Container>
         <h2>
-          <Link to="/" className="link-light text-decoration-none">ChatApp</Link>
+          <Link to="/" className="link-light text-decoration-none">
+            ChatApp
+          </Link>
         </h2>
-        <span className="text-success">Logged in as Sagar</span>
+        {user && <span className="text-success">Logged in as {user?.name}</span>}
         <Nav>
-            <Stack direction="horizontal" gap={3}>
-                <Link to="/login" className="link-light text-decoration-none">Login</Link>
-                <Link to="/register" className="link-light text-decoration-none">Register</Link>
-            </Stack>
+          <Stack direction="horizontal" gap={3}>
+            {user && (
+              <>
+                <Link
+                  to="/login"
+                  className="link-light text-decoration-none"
+                  onClick={() => logoutUser()}
+                >
+                  Logout
+                </Link>
+              </>
+            )}
+            {!user && (
+              <>
+                <Link to="/login" className="link-light text-decoration-none">
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="link-light text-decoration-none"
+                >
+                  Register
+                </Link>
+              </>
+            )}
+          </Stack>
         </Nav>
       </Container>
     </Navbar>
