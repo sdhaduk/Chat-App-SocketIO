b3bee1dd804b6dcc8a8dd90528867479058e21a8
sdhaduk <sagardhaduk1@gmail.com>
2023-11-21 16:43:24 -0600

    added getRequest method to util

--- frontend/src/utils/services.ts
+++ frontend/src/utils/services.ts
@@ -21,3 +21,20 @@ export const postRequest = async (url: string, body: any) => {
 
   return data;
 };
+
+export const getRequest = async (url: string) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (!response.ok) {
+    let message = "An error occurred...";
+
+    if (data?.message) {
+      message = data.message;
+    }
+
+    return { error: true, message };
+  }
+
+  return data;
+};
