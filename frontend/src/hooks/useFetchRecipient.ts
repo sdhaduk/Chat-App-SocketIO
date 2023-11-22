a14b1bb0c79686ba8984ed230628fe865ed06fdb
sdhaduk <sagardhaduk1@gmail.com>
2023-11-21 20:25:10 -0600

    added custom hook to get user recieving the messag

---
+++ frontend/src/hooks/useFetchRecipient.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { ChatType, RecipientUser, User } from "../types/types";
+import { baseUrl, getRequest } from "../utils/services";
+
+export const useFetchRecipient = (chat: ChatType, user: User) => {
+  const [recipientUser, setRecipientUser] = useState<RecipientUser | null>(null);
+  const [error, setError] = useState(null);
+
+  const recipientId = chat?.members.find((id) => id !== user?._id)
+
+  useEffect(() => {
+    const getUser = async () => {
+        if(!recipientId) return null
+
+        const response = await getRequest(`${baseUrl}/users/find/${recipientId}`)
+
+        if(response.error) {
+            return setError(error)
+        }
+
+        setRecipientUser(response);
+    }
+
+    getUser();
+  }, [])
+
+  return {recipientUser}
+};
