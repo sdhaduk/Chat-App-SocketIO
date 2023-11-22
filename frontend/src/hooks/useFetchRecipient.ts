5cf9f9d01917360e0470bdc058fc60b1364c6c80
sdhaduk <sagardhaduk1@gmail.com>
2023-11-22 16:56:36 -0600

    added dependency to auto update recipientId

--- frontend/src/hooks/useFetchRecipient.ts
+++ frontend/src/hooks/useFetchRecipient.ts
@@ -3,26 +3,28 @@ import { ChatType, RecipientUser, User } from "../types/types";
 import { baseUrl, getRequest } from "../utils/services";
 
 export const useFetchRecipient = (chat: ChatType, user: User) => {
-  const [recipientUser, setRecipientUser] = useState<RecipientUser | null>(null);
-  const [error, setError] = useState(null);
+  const [recipientUser, setRecipientUser] = useState<RecipientUser | null>(
+    null
+  );
+  const [error, setError] = useState<object | null>(null);
 
-  const recipientId = chat?.members.find((id) => id !== user?._id)
+  const recipientId = chat?.members?.find((id) => id !== user?._id);
 
   useEffect(() => {
     const getUser = async () => {
-        if(!recipientId) return null
+      if (!recipientId) return null;
 
-        const response = await getRequest(`${baseUrl}/users/find/${recipientId}`)
+      const response = await getRequest(`${baseUrl}/users/find/${recipientId}`);
 
-        if(response.error) {
-            return setError(error)
-        }
+      if (response.error) {
+        return setError(error);
+      }
 
-        setRecipientUser(response);
-    }
+      setRecipientUser(response);
+    };
 
     getUser();
-  }, [])
+  }, [recipientId]);
 
-  return {recipientUser}
+  return { recipientUser };
 };
