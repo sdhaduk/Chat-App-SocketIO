9fa72171f7a9f1803265639e8be8e6949410906a
sdhaduk <sagardhaduk1@gmail.com>
2023-11-21 20:26:59 -0600

    Added Chat page and UserChat component

---
+++ frontend/src/components/chat/UserChat.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { ChatType, User } from "../../types/types";
+import { useFetchRecipient } from "../../hooks/useFetchRecipient";
+import { Stack } from "react-bootstrap";
+import profile from "../../assets/profile.svg";
+
+interface Props {
+  chat: ChatType;
+  user: User;
+}
+
+const UserChat: React.FC<Props> = ({ chat, user }: Props) => {
+  const { recipientUser } = useFetchRecipient(chat, user);
+
+  return (
+    <Stack
+      direction="horizontal"
+      gap={3}
+      className="user-card align-items-center p-2 justify-content-between"
+      role = "button" 
+    >
+      <div className="d-flex">
+        <div className="me-2">
+          <img src={profile} height="40px" />
+        </div>
+        <div className="text-content">
+        <div className="name">{recipientUser?.name}</div>
+        <div className="text">Messages</div>
+        </div>
+      </div>
+      <div className="d-flex flex-column align-items-end">
+        <div className="date">12/12/2012</div>
+        <div className="this-user-notifications">2</div>
+        <span className="user-online"></span>
+      </div>
+    </Stack>
+  );
+};
+
+export default UserChat;
--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { Container, Stack } from "react-bootstrap";
+import UserChat from "../components/chat/UserChat";
+import { AuthContext } from "../context/AuthContext";
+import { ChatType } from "../types/types";
 
 const Chat: React.FC = () => {
-  return <>Chat</>;
+  const { user } = useContext(AuthContext);
+  const { userChats, isUserChatsLoading, userChatsError } =
+    useContext(ChatContext);
+
+  return (
+    <Container>
+      {userChats?.length < 1 ? null : (
+        <Stack direction="horizontal" gap={4} className="align-items-start">
+          <Stack className="flex-grow-0 messages-box pe-3" gap={3}>
+            {isUserChatsLoading && <p>Loading chats...</p>}
+            {userChats?.map((chat: ChatType, index: number) => {
+              return (
+                <div key={index}>
+                  <UserChat chat={chat} user={user} />
+                </div>
+              );
+            })}
+          </Stack>
+          <p>ChatBox</p>
+        </Stack>
+      )}
+    </Container>
+  );
 };
 
 export default Chat;
