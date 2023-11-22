8bdfaef0be7ea094145692e28d21ed5350742be8
sdhaduk <sagardhaduk1@gmail.com>
2023-11-22 01:30:43 -0600

    added functionality to create chat with any user

--- frontend/src/context/ChatContext.tsx
+++ frontend/src/context/ChatContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { baseUrl, getRequest, postRequest } from "../utils/services.ts";
-import { User } from "../types/types.ts";
+import { User, ChatType } from "../types/types.ts";
 
 interface Props {
   children: React.ReactNode;
@@ -13,9 +13,38 @@ export const ChatContextProvider: React.FC<Props> = ({
   children,
   user,
 }: Props) => {
-  const [userChats, setUserChats] = useState(null);
+  const [userChats, setUserChats] = useState<ChatType[] | null>(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState<boolean>(false);
   const [userChatsError, setUserChatsError] = useState<object | null>(null);
+  const [potentialChats, setPotentialChats] = useState<User[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await getRequest(`${baseUrl}/users`);
+
+      if (response.error) {
+        return console.log(response.error);
+      }
+
+      const pChats = response.filter((u: User) => {
+        let isChatCreated = false;
+
+        if (user._id === u._id) return false;
+
+        if (userChats) {
+          isChatCreated = userChats?.some((chat) => {
+            return chat.members[0] === u._id || chat.members[1] === u._id;
+          });
+        }
+
+        return !isChatCreated;
+      });
+
+      setPotentialChats(pChats);
+    };
+
+    getUsers();
+  }, [userChats]);
 
   useEffect(() => {
     const getUserChats = async () => {
@@ -45,6 +74,7 @@ export const ChatContextProvider: React.FC<Props> = ({
           userChats,
           isUserChatsLoading,
           userChatsError,
+          potentialChats,
         }}
       >
         {children}
