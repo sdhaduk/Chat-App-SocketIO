4a62eeb8ff327f66af09e7cdbee748bdfc5fb1b2
sdhaduk <sagardhaduk1@gmail.com>
2023-11-21 20:25:36 -0600

    Added Chat and RecipientUser type

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,19 +1,34 @@
-    interface Register {
-    name: string;
-    email: string;
-    password: string;
-  }
-  
-  interface User {
-    _id: string;
-    name: string;
-    email: string;
-    token: string;
-  }
-  
-  interface Login {
-    email: string;
-    password: string;
-  }
+interface Register {
+  name: string;
+  email: string;
+  password: string;
+}
 
-  export type {Register as Register, User as User, Login as Login}
+interface User {
+  _id: string;
+  name: string;
+  email: string;
+  token: string;
+}
+
+interface Login {
+  email: string;
+  password: string;
+}
+
+interface Chat {
+  _id: string;
+  members: Array<string>;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface RecipientUser {
+  _id: string;
+  createdAt: Date;
+  email: string;
+  name: string;
+  updatedAt: string;
+}
+
+export type { Register, User, Login, Chat as ChatType, RecipientUser };
